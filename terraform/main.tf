91df13ab8b2738c971a4b7f5123e4b5a24326902
Mustafa Mohamoud <mustafaabukar13@gmail.com>
2026-07-12 15:45:53 +0100

    debugged main root added private rt in module vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,7 @@ module "vpc" {
   public_subnet_cidrs  = var.public_subnet_cidrs
   private_subnet_cidrs = var.private_subnet_cidrs
   azs                  = var.azs
+  private_rt = var.private_rt
 }
 
 module "rds" {
@@ -29,5 +30,7 @@ module "endpoint" {
   source = "./modules/endpoint"
 
   vpc_id = module.vpc.vpc_id
+  private_rt = module.vpc.private_rt
+  
   
 }
