5f34e6e010b9b9e0006fac2a2557b684318b7aaf
Mustafa Mohamoud <mustafaabukar13@gmail.com>
2026-07-12 14:00:20 +0100

    variable.tf created + tag name for resource block s3 endpoint changed

--- terraform/modules/endpoint/main.tf
+++ terraform/modules/endpoint/main.tf
@@ -1,8 +1,8 @@
-resource "aws_vpc_endpoint" "s3" {
+resource "aws_vpc_endpoint" "s3 endpoint" {
   vpc_id       = aws_vpc.main.id
   service_name = "com.amazonaws.us-west-2.s3"
 
   tags = {
-    Environment = "test"
+    name = "s3 endpoint"
   }
 }
