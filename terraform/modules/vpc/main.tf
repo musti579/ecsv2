2c597cf7f75427135be2a9d63f6939d0fa1f3dc2
Mustafa Mohamoud <mustafaabukar13@gmail.com>
2026-07-12 13:37:16 +0100

    interface endpoint needed in order for dns resolution to work

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,7 +1,8 @@
 resource "aws_vpc" "vpc" {
   cidr_block       = var.vpc_cidr
   instance_tenancy = "default"
-
+  enable_dns_hostnames = true
+  enable_dns_support = true
   tags = {
     Name = "ecs2_vpc"
   }
